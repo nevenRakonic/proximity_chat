cba94e44e21967c4e4842e5b5a514d4e776ba0d7
NevenRakonic <neven.rakonic@gmail.com>
2015-07-23 21:57:30 +0200

    fix room model and move bootstrap to vendor folder

--- web/models/room.ex
+++ web/models/room.ex
@@ -1,7 +1,7 @@
 defmodule ProximityChat.Room do
   import ProximityChat.RedisHelper
 
-  def create_room(room_id, user_id), do
+  def create_room(room_id, user_id) do
     redis_query ["SADD", "rooms:" <> room_id, user_id]
   end
 
@@ -9,8 +9,9 @@ defmodule ProximityChat.Room do
     redis_query ["DEL", "rooms:" <> room_id ]
   end
 
-  def remove_user_from_room(room_id, user_id)
-    redis_query("SREM", "rooms:" <> room_id, user_id )
+  def remove_user_from_room(room_id, user_id) do
+    redis_query ["SREM", "rooms:" <> room_id, user_id]
+  end
 
   def room_size(room_id) do
     redis_query ["SCARD", "rooms:" <> room_id ]
